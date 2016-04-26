bcb25df15a3c1cd5f87844d6dced02347fcb9fa3
frankfarmer <frank@wikia-inc.com>
2016-04-26 23:37:12 +0000

    cleanup

--- ghetto_deb_build.sh
+++ ghetto_deb_build.sh
@@ -1,4 +1,5 @@
-# sudo apt-get install apache2-dev 
-apxs -c mod_realdoc.c 
-debuild -us -uc -b 
+#!/bin/bash
+# sudo apt-get install apache2-dev
+apxs -c mod_realdoc.c
+debuild -us -uc -b
 ls -larht ../libapache*deb
